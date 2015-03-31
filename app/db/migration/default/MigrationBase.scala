c0f8a6276355e462e4b9c984bd849da0236f79ca
mystelynx <me@urau.la>
2015-04-01 00:14:09 +0900

    migrate処理をDSL化してみた

---
+++ app/db/migration/default/MigrationBase.scala
@@ -0,0 +1,24 @@
+package db.migration.default
+
+import java.sql.Connection
+
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration
+import play.api.Logger
+import scalikejdbc._
+
+/**
+ * Created by tomohiro_urakawa on 2015/03/31.
+ */
+trait MigrationBase extends JdbcMigration {
+
+  // do not show migration logs
+  GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(enabled = false)
+
+  def migrateAlways(block: => Unit)(implicit session: DBSession) = block
+
+  def migrateOn(product: Symbol)(block: => Unit)(implicit session: DBSession) =
+    if (product == session.connection.getMetaData.getDatabaseProductName)
+      block
+    else
+      Logger.info(s"product unmatched. expected: $product actual: ${session.connection.getMetaData.getDatabaseProductName}")
+}
--- app/db/migration/default/V1__initial.scala
+++ app/db/migration/default/V1__initial.scala
@@ -3,14 +3,11 @@ package db.migration.default
 import java.sql.Connection
 import scalikejdbc._
 
-import org.flywaydb.core.api.migration.jdbc.JdbcMigration
+class V1__initial extends MigrationBase {
 
-class V1__initial extends JdbcMigration {
+  override def migrate(connection: Connection) = DB(connection) withinTx { implicit session =>
 
-  // migrationのSQLは非表示で良い
-  GlobalSettings.loggingSQLAndTime = LoggingSQLAndTimeSettings(enabled = false)
-
-  override def migrate(connection: Connection): Unit = DB(connection) withinTx { implicit session =>
+    migrateAlways {
 
       sql"""
       create table accounts(
@@ -31,29 +28,31 @@ class V1__initial extends JdbcMigration {
         deleted_at timestamp
       );
       """.update.apply
+    }
 
-      if (connection.getMetaData.getDatabaseProductName == "PostgreSQL") {
-        sql"""
-        create or replace function set_current_account() returns trigger AS $$$$
+    migrateOn('PostgreSQL) {
+
+      sql"""
+      create or replace function set_current_account() returns trigger AS $$$$
         BEGIN
           delete from accounts where id = NEW.account_id;
           insert into accounts(id, name, password)
-          values (NEW.account_id, NEW.name, NEW.password);
+            values (NEW.account_id, NEW.name, NEW.password);
 
           return NEW;
         END;
-        $$$$ LANGUAGE 'plpgsql';
-        """.update.apply
+      $$$$ LANGUAGE 'plpgsql';
+      """.update.apply
 
-        sql"""
-        drop trigger if EXISTS current_account on account_updates;
-        """.update.apply
+      sql"""
+      drop trigger if EXISTS current_account on account_updates;
+      """.update.apply
 
-        sql"""
-        create trigger current_account after insert on account_updates
-          for each ROW
-          execute procedure set_current_account();
-        """.update.apply
+      sql"""
+      create trigger current_account after insert on account_updates
+        for each ROW
+        execute procedure set_current_account();
+      """.update.apply
     }
   }
 }
